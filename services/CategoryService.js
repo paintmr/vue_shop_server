acf160243cda890d5da3dbb77b2a7a40eba3c359
chujingxl <chujingxl@gmail.com>
2021-09-27 23:21:30 +0800

    services/CategoryService.js和routes/api/private/v1/categories.js中都修改了编辑商品类别相关的函数updateCategory的参数。之前的版本无法修改父层级id和本身的层级，只能修改商品类别名称

--- services/CategoryService.js
+++ services/CategoryService.js
@@ -1,6 +1,6 @@
 var _ = require('lodash');
 var path = require("path");
-var dao = require(path.join(process.cwd(),"dao/DAO"));
+var dao = require(path.join(process.cwd(), "dao/DAO"));
 
 /**
  * 判断是否删除
@@ -9,15 +9,15 @@ var dao = require(path.join(process.cwd(),"dao/DAO"));
  * @param  {[type]}  cat           [description]
  * @return {Boolean}               [description]
  */
-function isDelete(keyCategories,cat) {
-	if(cat.cat_pid == 0) {
+function isDelete(keyCategories, cat) {
+	if (cat.cat_pid == 0) {
 		return cat.cat_deleted;
-	} else if(cat.cat_deleted) {
+	} else if (cat.cat_deleted) {
 		return true;
 	} else {
 		parentCat = keyCategories[cat.cat_pid];
-		if(!parentCat) return true;
-		return isDelete(keyCategories,parentCat);
+		if (!parentCat) return true;
+		return isDelete(keyCategories, parentCat);
 	}
 }
 
@@ -26,20 +26,20 @@ function isDelete(keyCategories,cat) {
  * @param  {[type]} keyCategories [description]
  * @return {[type]}               [description]
  */
-function getTreeResult(keyCategories,categories,type) {
-	
+function getTreeResult(keyCategories, categories, type) {
+
 	var result = [];
-	for(idx in categories) {
+	for (idx in categories) {
 		var cat = categories[idx];
 		// 判断是否被删除
-		if(isDelete(keyCategories,cat)) continue;
-		if(cat.cat_pid == 0) {
+		if (isDelete(keyCategories, cat)) continue;
+		if (cat.cat_pid == 0) {
 			result.push(cat);
 		} else {
-			if(cat.cat_level >= type) continue;
+			if (cat.cat_level >= type) continue;
 			var parantCat = keyCategories[cat.cat_pid];
-			if(!parantCat) continue;
-			if(!parantCat.children) {
+			if (!parantCat) continue;
+			if (!parantCat.children) {
 				parantCat["children"] = [];
 			}
 			parantCat.children.push(cat);
@@ -57,26 +57,26 @@ function getTreeResult(keyCategories,categories,type) {
  * @param  {[type]}   type    描述显示层级
  * @param  {Function} cb      回调函数
  */
-module.exports.getAllCategories = function(type,conditions,cb) {
-	dao.list("CategoryModel",{"cat_deleted":false},function(err,categories) {
-		var keyCategories = _.keyBy(categories,'cat_id');
-		if(!type) type = 3;
+module.exports.getAllCategories = function (type, conditions, cb) {
+	dao.list("CategoryModel", { "cat_deleted": false }, function (err, categories) {
+		var keyCategories = _.keyBy(categories, 'cat_id');
+		if (!type) type = 3;
 
-		result = getTreeResult(keyCategories,categories,type);
-		
-		if(conditions) {
+		result = getTreeResult(keyCategories, categories, type);
+
+		if (conditions) {
 			count = result.length;
 			pagesize = parseInt(conditions.pagesize);
 			pagenum = parseInt(conditions.pagenum) - 1;
-			result = _.take(_.drop(result,pagenum * pagesize),pagesize)
+			result = _.take(_.drop(result, pagenum * pagesize), pagesize)
 			var resultDta = {};
 			resultDta["total"] = count;
 			resultDta["pagenum"] = pagenum;
 			resultDta["pagesize"] = pagesize;
 			resultDta["result"] = result;
-			return cb(null,resultDta);
+			return cb(null, resultDta);
 		}
-		cb(null,result);
+		cb(null, result);
 	});
 }
 
@@ -86,10 +86,10 @@ module.exports.getAllCategories = function(type,conditions,cb) {
  * @param  {[type]}   id 分类ID
  * @param  {Function} cb 回调函数
  */
-module.exports.getCategoryById = function(id,cb) {
-	dao.show("CategoryModel",id,function(err,category){
-		if(err) return cb("获取分类对象失败");
-		cb(null,category);
+module.exports.getCategoryById = function (id, cb) {
+	dao.show("CategoryModel", id, function (err, category) {
+		if (err) return cb("获取分类对象失败");
+		cb(null, category);
 	})
 }
 
@@ -105,10 +105,10 @@ module.exports.getCategoryById = function(id,cb) {
  * 
  * @param {Function} cb  回调函数
  */
-module.exports.addCategory = function(cat,cb) {
-	dao.create("CategoryModel",{"cat_pid":cat.cat_pid,"cat_name":cat.cat_name,"cat_level":cat.cat_level},function(err,newCat) {
-		if(err) return cb("创建分类失败");
-		cb(null,newCat);
+module.exports.addCategory = function (cat, cb) {
+	dao.create("CategoryModel", { "cat_pid": cat.cat_pid, "cat_name": cat.cat_name, "cat_level": cat.cat_level }, function (err, newCat) {
+		if (err) return cb("创建分类失败");
+		cb(null, newCat);
 
 	});
 }
@@ -120,10 +120,10 @@ module.exports.addCategory = function(cat,cb) {
  * @param  {[type]}   newName 新的名称
  * @param  {Function} cb      回调函数
  */
-module.exports.updateCategory = function(cat_id,newName,cb) {
-	dao.update("CategoryModel",cat_id,{"cat_name":newName},function(err,newCat) {
-		if(err) return cb("更新失败");
-		cb(null,newCat);
+module.exports.updateCategory = function (cat_id, newCate, cb) {
+	dao.update("CategoryModel", cat_id, { "cat_name": newCate.cate_name, "cat_pid": newCate.cate_pid, "cat_level": newCate.cate_level }, function (err, newCat) {
+		if (err) return cb("更新失败");
+		cb(null, newCat);
 	});
 }
 
@@ -133,10 +133,10 @@ module.exports.updateCategory = function(cat_id,newName,cb) {
  * @param  {[type]}   cat_id 分类ID
  * @param  {Function} cb     回调函数
  */
-module.exports.deleteCategory = function(cat_id,cb) {
-	dao.update("CategoryModel",cat_id,{"cat_deleted":true},function(err,newCat){
+module.exports.deleteCategory = function (cat_id, cb) {
+	dao.update("CategoryModel", cat_id, { "cat_deleted": true }, function (err, newCat) {
 
-		if(err) return cb("删除失败");
+		if (err) return cb("删除失败");
 		cb("删除成功");
 	});
 }
